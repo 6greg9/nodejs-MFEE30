96073f030a2009e3c7c745737563d7f5b702652d
greg <greg4253058@gmail.com>
2021-05-23 14:33:54 +0800

    quiz

---
+++ basic/quiz.js
@@ -0,0 +1,41 @@
+console.log("start");
+
+(function () {
+  console.log("IIFE");
+  setTimeout(function () {
+    console.log("Timeout");
+  }, 1000);
+})();
+
+console.log("end");
+
+console.log("start");
+
+(function () {
+  console.log("IIFE");
+  setTimeout(function () {
+    console.log("Timeout");
+  }, 0);
+})();
+
+console.log("end");
+
+const bar = () => console.log("bar");
+
+const baz = () => console.log("baz");
+
+const foo = () => {
+    console.log("foo");
+    bar();
+    baz();
+};
+
+foo();
+
+const foo2 = () => {
+    console.log("foo");
+    setTimeout(bar, 0);
+    baz();
+};
+
+foo2();
